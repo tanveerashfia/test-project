6d51c1ee2e983fac4f00a84260fe038f4eac9fbc
Ashfia Tanveer <bsef21m025@pucit.edu.pk>
2022-10-16 07:48:55 -0700

    Initial Commit

---
+++ 1.cpp
@@ -0,0 +1,35 @@
+//#include <iostream>
+//using namespace std;
+//void perfect(int);
+//int main()
+//{
+//	int N1;
+//	cout << "Enter a num: ";
+//	cin >> N1;
+//	perfect(N1);
+//	return 0;
+//}
+//void perfect(int N1)
+//{
+//	int sum = 0, i;
+//	for (i = 1; i < N1; i++)
+//	{
+//		if (N1 % i == 0)
+//		{
+//			sum = sum + i;
+//		}
+//	}
+//	if (sum == N1)
+//	{
+//		cout << "It is a perfect number";
+//	}
+//	else
+//	{
+//		cout << "It is not a perfect number";
+//	}
+//}
+//
+//
+//	
+//
+//	
